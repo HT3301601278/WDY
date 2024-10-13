84d69911015a58efcc6c0f554e5bef5805e62d25
世间微斯人 <3301601278@qq.com>
2024-10-13 20:55:54 +0800

    更新 constants.dart

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  // 使用10.0.2.2代替localhost,这是Android模拟器访问主机的特殊IP
-  static const String baseUrl = 'http://192.168.193.98:8080';
+  // 使用http://10.0.2.2:8080代替localhost,这是Android模拟器访问主机的特殊IP
+  static const String baseUrl = 'http://192.168.3.98:8080';
   static String token = ''; // 用于存储登录后的token
 }
